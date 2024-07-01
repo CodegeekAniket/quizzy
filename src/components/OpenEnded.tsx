1b37b255e3136bf1e45a82ac1fe94eec32a7efa0
Aniket <k.aniket9811@gmail.com>
2024-07-01 23:55:10 +0530

    statistics page completed

--- src/components/OpenEnded.tsx
+++ src/components/OpenEnded.tsx
@@ -1,17 +1,18 @@
 'use client'
-import { formatTimeDelta } from '@/lib/utils'
+import { cn, formatTimeDelta } from '@/lib/utils'
 import { Game, Question } from '@prisma/client'
 import { differenceInSeconds } from 'date-fns'
-import { ChevronsRight, Loader2, Timer } from 'lucide-react'
+import { BarChart, ChevronsRight, Loader2, Timer } from 'lucide-react'
 import React from 'react'
 import { Card, CardDescription, CardHeader, CardTitle } from './ui/card'
-import { Button } from './ui/button'
+import { Button, buttonVariants } from './ui/button'
 import { useToast } from './ui/use-toast'
 import { useMutation } from '@tanstack/react-query'
 import { z } from 'zod'
 import { checkAnswerSchema } from '@/schemas/form/quiz'
 import axios from 'axios'
 import BlankAnswerInput from './BlankAnswerInput'
+import Link from "next/link"
 
 type Props = {
     game : Game & {questions :Pick<Question,'id' | 'question' | 'answer'> []}
@@ -20,7 +21,7 @@ type Props = {
 const OpenEnded = ({game}: Props) => {
     const [questionIndex, setQuestionIndex] = React.useState(0);
     const [blankAnswer,setBlankAnswer] = React.useState<string>("")
-    const [hasEnded,setHasEnded] = React.useState<boolean>(false);
+    const [hasEnded,setHasEnded] = React.useState(false);
     const[now,setNow]=React.useState<Date>(new Date()) 
     const{toast} = useToast()
 
@@ -62,7 +63,7 @@ const OpenEnded = ({game}: Props) => {
             onSuccess :({percentageSimilar}) =>{
                 toast({
                     title:`Your Answer is ${percentageSimilar} % similar to the correct answer.`,
-                    description:"answer are matched based on similarity comparisons",
+                    description:"Answers are matched based on similarity comparisons",
                 })
                 if(questionIndex === game.questions.length -1){
                     setHasEnded(true);
@@ -85,6 +86,21 @@ const OpenEnded = ({game}: Props) => {
         };
     },[handleNext]);
 
+    if(hasEnded){
+        return(
+            <div className='absolute flex flex-col justify-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                <div className='px-4 mt-2 font-semibold text-white bg-green-500 rounded-md whitespace-nowrap'>
+                    You completed in {" "}
+                    {formatTimeDelta(differenceInSeconds(now,game.timeStarted))}
+                </div>
+                <Link href={`/statistics/${game.id}`} className={cn(buttonVariants(),"mt-2")}>
+                View Statistics
+                <BarChart className='w-4 h-4 ml-2'/>
+                </Link>
+            </div>
+        )
+    }
+    
     return (
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-{80vw} max-w-4xl w-{90vw} ">
             <div className="flex flex-row justify-between">
